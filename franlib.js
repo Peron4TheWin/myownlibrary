e7ef0528338e6902e66890a09e3d9c2a86f0e846
Peron4TheWin <frandonna12@gmail.com>
2023-09-18 19:47:32 -0300

    changed

--- franlib.js
+++ franlib.js
@@ -1,11 +1,10 @@
 /**
- * [Funcion obtener]
+ * [Obtiene un elemento por el ID]
  * @param {[String]} id [El id del elemento a obtener]
- * @returns {[]}
+ * @returns {[Object]} 
  */
 
 function obtener(id){
     return document.getElementById(id)
 }
-//a
 
